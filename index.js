44d159784861d5360043d6c77b2d5bcbd596f04a
bcaudillo <btcaudillo@gmail.com>
2022-03-31 17:48:18 -0600

    commit tree 9c70d51e1a27f190f50bbd5d7d8603e8c6470ae5

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let newArray=[];
+let i = 0;
+function writeCards(names,event){
+    for (i;i<names.length; i++){
+        newArray[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    }
+return newArray
+}
+
+function countDown(integer){
+    while (integer > -1){
+        console.log(integer)
+        integer--
+    }
+}
